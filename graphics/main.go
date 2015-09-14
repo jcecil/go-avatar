5dff0247e6d8109e0048272b69b004266b1fded0
Jerry Belton <jerry.belton@gmail.com>
2015-09-13 20:09:57 -0700

    Full opengl4 example is running now. It's using opengl3.2

--- graphics/main.go
+++ graphics/main.go
@@ -1,14 +1,18 @@
 package graphics
 
 import (
-	//	game "bitbucket.org/jcecil/avatar/game"
 	fmt "fmt"
-	//	"github.com/go-gl/gl"
 	gl "github.com/go-gl/gl/v4.1-core/gl"
 	glfw "github.com/go-gl/glfw3/v3.1/glfw"
-
-	//	"github.com/go-gl/mathgl/mgl32"
-	"runtime"
+	mgl32 "github.com/go-gl/mathgl/mgl32"
+	//log "log"
+	errors "errors"
+	"image"
+	"image/draw"
+	_ "image/png"
+	os "os"
+	runtime "runtime"
+	strings "strings"
 )
 
 const (
@@ -16,23 +20,6 @@ const (
 	WindowWidth  = 1024
 	WindowHeight = 768
 )
-
-//var (
-//	window       *glfw.Window
-//	err          error
-//	rotx, roty   float32
-//	vertexBuffer gl.Buffer
-//	colorBuffer  gl.Buffer
-//	programID    gl.Program
-//	matrixID     gl.UniformLocation
-//	mvp          mgl32.Mat4
-//	colorBufferData []gl.GLfloat
-//	loopCount       int        = 1
-//	position        mgl32.Vec3 = [...]float32{0, 0, 5}
-//	horizontalAngle float32    = 3.14
-//	verticalAngle   float32    = 0
-//	fieldOfView     float32    = 45
-//)
 
 func init() {
 	runtime.LockOSThread()
@@ -49,8 +36,8 @@ func Main() {
 	defer glfw.Terminate()
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
-	//glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	//glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 2)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 	window, err := glfw.CreateWindow(WindowWidth, WindowHeight, "Cube", nil, nil)
@@ -67,113 +54,256 @@ func Main() {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
+	// Configure the vertex and fragment shaders
+	program, err := newProgram(vertexShader, fragmentShader)
+	if err != nil {
+		panic(err)
+	}
+	gl.UseProgram(program)
+
+	projection := mgl32.Perspective(mgl32.DegToRad(45.0), float32(WindowWidth)/WindowHeight, 0.1, 10.0)
+	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
+	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
+
+	camera := mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	cameraUniform := gl.GetUniformLocation(program, gl.Str("camera\x00"))
+	gl.UniformMatrix4fv(cameraUniform, 1, false, &camera[0])
+
+	model := mgl32.Ident4()
+	modelUniform := gl.GetUniformLocation(program, gl.Str("model\x00"))
+	gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+	textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
+	gl.Uniform1i(textureUniform, 0)
+
+	gl.BindFragDataLocation(program, 0, gl.Str("outputColor\x00"))
+
+	// Load the texture
+	texture, err := newTexture("square.png")
+	if err != nil {
+		panic(err)
+	}
+
+	// Configure the vertex data
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices)*4, gl.Ptr(cubeVertices), gl.STATIC_DRAW)
+
+	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
+	gl.EnableVertexAttribArray(vertAttrib)
+	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+
+	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord\x00")))
+	gl.EnableVertexAttribArray(texCoordAttrib)
+	gl.VertexAttribPointer(texCoordAttrib, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+
+	// Configure global settings
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+
+	angle := 0.0
+	previousTime := glfw.GetTime()
+
 	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		// Update
+		time := glfw.GetTime()
+		elapsed := time - previousTime
+		previousTime = time
+
+		angle += elapsed
+		model = mgl32.HomogRotate3D(float32(angle), mgl32.Vec3{0, 1, 0})
+
+		// Render
+		gl.UseProgram(program)
+		gl.UniformMatrix4fv(modelUniform, 1, false, &model[0])
+
+		gl.BindVertexArray(vao)
+
+		gl.ActiveTexture(gl.TEXTURE0)
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+
+		gl.DrawArrays(gl.TRIANGLES, 0, 6*2*3)
+
+		// Maintenance
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
 }
 
-//	if !glfw.Init() {
-//		panic("Can't init glfw!")
-//	}
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
 
-// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-1-opening-a-window/
-// 4x antialiasing
-//	glfw.WindowHint(glfw.Samples, 4)
-//	glfw.WindowHint(glfw.ContextVersionMajor, 3)
-//	glfw.WindowHint(glfw.ContextVersionMinor, 3)
-//	glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
 
-//	window, err = glfw.CreateWindow(Width, Height, Title, nil, nil)
-//	if err != nil {
-//		panic(err)
-//	}
+	program := gl.CreateProgram()
 
-//	window.MakeContextCurrent()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
 
-//	glfw.SwapInterval(1)
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
 
-//	gl.Init()
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
 
-// Triangle draw
-//	vertexArray := gl.GenVertexArray()
-//	vertexArray.Bind()
+		return 0, errors.New(fmt.Sprintf("failed to link program: %v", log))
+	}
 
-// Triangle
-//vertexBufferData := []gl.GLfloat{-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0}
-// Cube
-//	vertexBufferData := []gl.GLfloat{-1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0}
-//	colorBufferData = []gl.GLfloat{-1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0}
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
 
-//	vertexBuffer = gl.GenBuffer()
-//	vertexBuffer.Bind(gl.ARRAY_BUFFER)
-//	gl.BufferData(gl.ARRAY_BUFFER, len(vertexBufferData)*4, vertexBufferData, gl.STATIC_DRAW)
+	return program, nil
+}
 
-//	colorBuffer = gl.GenBuffer()
-//	colorBuffer.Bind(gl.ARRAY_BUFFER)
-//	gl.BufferData(gl.ARRAY_BUFFER, len(colorBufferData)*4, colorBufferData, gl.STATIC_DRAW)
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
 
-//	gl.ClearColor(0.0, 0.0, 0.3, 0.0)
+	csource := gl.Str(source)
+	gl.ShaderSource(shader, 1, &csource, nil)
+	gl.CompileShader(shader)
 
-//	programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader")
-//	matrixID = programID.GetUniformLocation("MVP")
-//	matrixID.UniformMatrix4f(false, &mvp)
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
 
-//Callbacks
-//	window.SetKeyCallback(onKey)
-//	window.SetCursorPositionCallback(onCursor)
-//	window.SetMouseButtonCallback(onMouse)
-//	fmt.Println("done!!!!!")
-//}
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
 
-//func Loop() bool {
-//	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-//	gl.Enable(gl.DEPTH_TEST)
-//	gl.DepthFunc(gl.LESS)
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
 
-// Model transformation
-//	model := mgl32.Ident4()
+	return shader, nil
+}
 
-// View transformation
-//	var eye mgl32.Vec3 = [...]float32{4, 3, 3}
-//	var center mgl32.Vec3 = [...]float32{0, 0, 0}
-//	var up mgl32.Vec3 = [...]float32{0, 1, 0}
-//	view := mgl32.LookAtV(eye, center, up)
+func newTexture(file string) (uint32, error) {
+	imgFile, err := os.Open(file)
+	if err != nil {
+		return 0, err
+	}
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		return 0, err
+	}
 
-// Projection transformation
-//	projection := mgl32.Perspective(45, 4.0/3.0, .1, 100)
-//
-//	mvp = projection.Mul4(view).Mul4(model)
+	rgba := image.NewRGBA(img.Bounds())
+	if rgba.Stride != rgba.Rect.Size().X*4 {
+		return 0, fmt.Errorf("unsupported stride")
+	}
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
 
-//	programID.Use()
-//	matrixID.UniformMatrix4fv(false, mvp)
+	var texture uint32
+	gl.GenTextures(1, &texture)
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.TexImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		int32(rgba.Rect.Size().X),
+		int32(rgba.Rect.Size().Y),
+		0,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		gl.Ptr(rgba.Pix))
 
-//	var vertexAttrib gl.AttribLocation = 0
-//	vertexAttrib.EnableArray()
-//	vertexBuffer.Bind(gl.ARRAY_BUFFER)
-//	vertexAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+	return texture, nil
+}
 
-//	colorBuffer.Bind(gl.ARRAY_BUFFER)
-//	loopCount = (loopCount + 1) % len(colorBufferData)
-//	for i := range colorBufferData {
-//		colorBufferData[i] = gl.GLfloat((float32((i + loopCount) % len(colorBufferData))) / float32(len(colorBufferData)))
-//	}
-//	gl.BufferData(gl.ARRAY_BUFFER, len(colorBufferData)*4, colorBufferData, gl.STATIC_DRAW)
+var vertexShader string = `
+#version 330
+uniform mat4 projection;
+uniform mat4 camera;
+uniform mat4 model;
+in vec3 vert;
+in vec2 vertTexCoord;
+out vec2 fragTexCoord;
+void main() {
+    fragTexCoord = vertTexCoord;
+    gl_Position = projection * camera * model * vec4(vert, 1);
+}
+` + "\x00"
 
-//	var colorAttrib gl.AttribLocation = 1
-//	colorAttrib.EnableArray()
-//	colorBuffer.Bind(gl.ARRAY_BUFFER)
-//	colorAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+var fragmentShader = `
+#version 330
+uniform sampler2D tex;
+in vec2 fragTexCoord;
+out vec4 outputColor;
+void main() {
+    outputColor = texture(tex, fragTexCoord);
+}
+` + "\x00"
 
-//gl.DrawArrays(gl.TRIANGLES, 0, 3)
-//	gl.DrawArrays(gl.TRIANGLES, 0, 36)
-//	vertexAttrib.DisableArray()
+var cubeVertices = []float32{
+	//  X, Y, Z, U, V
+	// Bottom
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
 
-//	glfw.PollEvents()
-//	window.SwapBuffers()
-//	return window.ShouldClose()
-//}
+	// Top
+	-1.0, 1.0, -1.0, 0.0, 0.0,
+	-1.0, 1.0, 1.0, 0.0, 1.0,
+	1.0, 1.0, -1.0, 1.0, 0.0,
+	1.0, 1.0, -1.0, 1.0, 0.0,
+	-1.0, 1.0, 1.0, 0.0, 1.0,
+	1.0, 1.0, 1.0, 1.0, 1.0,
 
-//func GetWindow() *glfw.Window {
-//	return window
-//}
+	// Front
+	-1.0, -1.0, 1.0, 1.0, 0.0,
+	1.0, -1.0, 1.0, 0.0, 0.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+	1.0, -1.0, 1.0, 0.0, 0.0,
+	1.0, 1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+
+	// Back
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	-1.0, 1.0, -1.0, 0.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	-1.0, 1.0, -1.0, 0.0, 1.0,
+	1.0, 1.0, -1.0, 1.0, 1.0,
+
+	// Left
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, -1.0, 1.0, 0.0,
+	-1.0, -1.0, -1.0, 0.0, 0.0,
+	-1.0, -1.0, 1.0, 0.0, 1.0,
+	-1.0, 1.0, 1.0, 1.0, 1.0,
+	-1.0, 1.0, -1.0, 1.0, 0.0,
+
+	// Right
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	1.0, -1.0, -1.0, 1.0, 0.0,
+	1.0, 1.0, -1.0, 0.0, 0.0,
+	1.0, -1.0, 1.0, 1.0, 1.0,
+	1.0, 1.0, -1.0, 0.0, 0.0,
+	1.0, 1.0, 1.0, 0.0, 1.0,
+}
